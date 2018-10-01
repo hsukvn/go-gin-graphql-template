773b976abd6a7915543cfe299cbd976d9b84bbe3
Kevin Hsu <hsukvn@gmail.com>
2018-10-01 11:29:40 +0800

    Add first example

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # go-graphql-template
+
 Basic file hierarchy of graphql API server in golang
+
+## Example
+
+curl -g 'http://localhost:9527/graphql?query={songs{title,duration}}'
---
+++ main.go
@@ -0,0 +1,150 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/graphql-go/graphql"
+)
+
+type Album struct {
+	ID     string `json:"id,omitempty"`
+	Artist string `json:"artist"`
+	Title  string `json:"title"`
+	Year   string `json:"year"`
+	Genre  string `json:"genre"`
+	Type   string `json:"type"`
+}
+
+type Artist struct {
+	ID   string `json:"id,omitempty"`
+	Name string `json:"name"`
+	Type string `json:"type"`
+}
+
+type Song struct {
+	ID       string `json:"id,omitempty"`
+	Album    string `json:"album"`
+	Title    string `json:"title"`
+	Duration string `json:"duration"`
+	Type     string `json:"type"`
+}
+
+var albums []Album = []Album{
+	Album{
+		ID:     "ts-fearless",
+		Artist: "1",
+		Title:  "Fearless",
+		Year:   "2008",
+		Type:   "album",
+	},
+}
+
+var artists []Artist = []Artist{
+	Artist{
+		ID:   "1",
+		Name: "Taylor Swift",
+		Type: "artist",
+	},
+}
+
+var songs []Song = []Song{
+	Song{
+		ID:       "1",
+		Album:    "ts-fearless",
+		Title:    "Fearless",
+		Duration: "4:01",
+		Type:     "song",
+	},
+	Song{
+		ID:       "2",
+		Album:    "ts-fearless",
+		Title:    "Fifteen",
+		Duration: "4:54",
+		Type:     "song",
+	},
+}
+
+func main() {
+	songType := graphql.NewObject(graphql.ObjectConfig{
+		Name: "Song",
+		Fields: graphql.Fields{
+			"id": &graphql.Field{
+				Type: graphql.String,
+			},
+			"album": &graphql.Field{
+				Type: graphql.String,
+			},
+			"title": &graphql.Field{
+				Type: graphql.String,
+			},
+			"duration": &graphql.Field{
+				Type: graphql.String,
+			},
+		},
+	})
+
+/*
+	artistType := graphql.NewObject(graphql.ObjectConfig{
+		Name: "Artist",
+		Fields: graphql.Fields{
+			"id": &graphql.Field{
+				Type: graphql.String,
+			},
+			"name": &graphql.Field{
+				Type: graphql.String,
+			},
+			"type": &graphql.Field{
+				Type: graphql.String,
+			},
+		},
+	})
+
+	albumType := graphql.NewObject(graphql.ObjectConfig{
+		Name: "Album",
+		Fields: graphql.Fields{
+			"id": &graphql.Field{
+				Type: graphql.String,
+			},
+			"artist": &graphql.Field{
+				Type: graphql.String,
+			},
+			"title": &graphql.Field{
+				Type: graphql.String,
+			},
+			"year": &graphql.Field{
+				Type: graphql.String,
+			},
+			"genre": &graphql.Field{
+				Type: graphql.String,
+			},
+			"type": &graphql.Field{
+				Type: graphql.String,
+			},
+		},
+	})
+*/
+	rootQuery := graphql.NewObject(graphql.ObjectConfig{
+		Name: "Query",
+		Fields: graphql.Fields{
+			"songs": &graphql.Field{
+				Type: graphql.NewList(songType),
+				Resolve: func(params graphql.ResolveParams) (interface{}, error) {
+					return songs, nil
+				},
+			},
+		},
+	})
+
+	schema, _ := graphql.NewSchema(graphql.SchemaConfig{Query: rootQuery})
+
+	http.HandleFunc("/graphql", func(w http.ResponseWriter, r *http.Request) {
+		result := graphql.Do(graphql.Params{
+			Schema: schema,
+			RequestString: r.URL.Query().Get("query"),
+		})
+		json.NewEncoder(w).Encode(result)
+	})
+
+	http.ListenAndServe(":9527", nil)
+}
